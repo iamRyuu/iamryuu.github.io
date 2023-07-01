df5b6101e64c42d1dcbd251b35dfcc306bc71457
Ryuuzaki <ryuuzaki.dev@gmail.com>
2023-07-01 15:35:32 -0600

    mapa

--- JavaScript/geo.js
+++ JavaScript/geo.js
@@ -6,6 +6,17 @@ function success(getCurrentPosition){
     alert('Su ubicación esta siendo compartida');
 
     let coords = GeolocationPosition.coords;
-    var lat =coords.lat;
-    var lng =coords.lng;
 }
+
+function iniciarMap(){
+    var coord = {lat: 12.110583708626425,lng: -86.34107404882353};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 14,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
+
