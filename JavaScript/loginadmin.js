c66684b07d836c16f29203b9d269ac5365c33b10
Ryuuzaki <ryuuzaki.dev@gmail.com>
2023-07-04 17:31:23 -0600

    validation buttons

--- JavaScript/loginadmin.js
+++ JavaScript/loginadmin.js
@@ -1,18 +1,32 @@
 const admin_mail = "admin@gmail.com"
 const password = "password123";
-
+const parrafo = document.getElementById("warnings")
+const mail = document.getElementById('mail');
+const pass = document.getElementById('pass');
 
 let login_button = document.getElementById('login-button');
 
 login_button.addEventListener('click', () => {
-    let mail = document.getElementById('mail');
-    let pass = document.getElementById('pass');
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
 
-    console.log(mail.value);
-    console.log(pass.value);
     if (mail.value == admin_mail && pass.value == password) {
         location.href = "../Pages/admin.html";
-    } else {
-        location.href = "../index.html";
-    }    
+    }
+    if(!regexEmail.test(mail.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+
 })
