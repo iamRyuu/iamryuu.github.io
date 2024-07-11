346d10e29fe854509f03c640fba5b130c4ad79ba
Jorge Obando <ryuuzaki.dev@gmail.com>
2024-07-11 14:20:12 -0600

    fix movil version

---
+++ JavaScript/admin/empleado.js
@@ -0,0 +1,27 @@
+let btnSend = document.getElementById('btnSend');
+const name = document.getElementById('name');
+const apellido = document.getElementById('Apellido');
+const ced = document.getElementById('ced');
+const num = document.getElementById('num');
+const input = document.querySelectorAll('input');
+
+btnSend.addEventListener('click' , ()=>{
+    if(!name.checkValidity()){
+        warnings.innerHTML = name.validationMessage;
+    }
+    if(!apellido.checkValidity()){
+        warnings1.innerHTML = apellido.validationMessage;
+    }
+    if(!ced.checkValidity()){
+        warnings2.innerHTML = ced.validationMessage;
+    }
+    if(!num.checkValidity()){
+        warnings3.innerHTML = num.validationMessage;
+    }
+
+    if(name.checkValidity() && Apellido.checkValidity() && ced.checkValidity() && num.checkValidity()){
+        input.forEach(input => input.value = '');
+        alert("Empleado agregado :D");
+    } 
+    
+})
